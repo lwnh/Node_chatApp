f5a5daddb875d6cd8a779c2b24214ea45d90861e
yumi <>
2021-01-20 18:47:33 +0900

    메세지 화면에 보여주기

--- client/src/Chatbot/Chatbot.js
+++ client/src/Chatbot/Chatbot.js
@@ -1,10 +1,12 @@
 import Axios from 'axios';
 import React, {useEffect} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { saveMessage } from '../_actions/message_actions'
+import { saveMessage } from '../_actions/message_actions';
+import Message from './Sections/Message';
 
 function Chatbot() {
     const dispatch = useDispatch();
+    const messagesFromRedux = useSelector(state => state.message.messages);
 
     useEffect(() => {
         eventQuery('welcomeToMyWebsite');
@@ -22,7 +24,6 @@ function Chatbot() {
             }
         }
         dispatch(saveMessage(conversation));
-        console.log('text I sent', conversation);
         
         // need to take care of the message Chatbot sent
         const textQueryVariables = {
@@ -87,13 +88,30 @@ function Chatbot() {
             e.target.value = "";
         }
     }
+
+    const renderOneMessage = (message, i) => {
+        console.log('message', message);
+
+        return <Message key={i} who={message.who} text={message.content.text.text} />
+    }
+
+    const renderMessage = (returnedMessages) => {
+        if(returnedMessages) {
+            return returnedMessages.map((message, i) => {
+                return renderOneMessage(message, i);
+            })
+        } else {
+            return null;
+        }
+    }
+    
     return (
         <div style={{ 
             height: 700, width: 700,
             border: '3px solid black', borderRadius: '7px'
         }}>
             <div style={{ height: 644, width: '100%', overflow: 'auto' }}>
-
+                {renderMessage(messagesFromRedux)}
             </div>
 
             <input
